4f58724dbd08082899221c8afa4c8ca940435bf3
Matt Jenkins <matt@majenko.co.uk>
2017-09-03 22:12:45 +0100

    Initial import

---
+++ Cambridge.h
@@ -0,0 +1,39 @@
+#ifndef _CAMBRIDGE_H
+#define _CAMBRIDGE_H
+
+#include <DisplayCore.h>
+
+namespace Fonts {
+    extern const uint8_t Cambridge10[];
+    extern const uint8_t Cambridge11[];
+    extern const uint8_t Cambridge12[];
+    extern const uint8_t Cambridge13[];
+    extern const uint8_t Cambridge14[];
+    extern const uint8_t Cambridge15[];
+    extern const uint8_t Cambridge16[];
+    extern const uint8_t Cambridge17[];
+    extern const uint8_t Cambridge18[];
+    extern const uint8_t Cambridge19[];
+    extern const uint8_t Cambridge20[];
+    extern const uint8_t Cambridge21[];
+    extern const uint8_t Cambridge22[];
+    extern const uint8_t Cambridge23[];
+    extern const uint8_t Cambridge24[];
+    extern const uint8_t Cambridge25[];
+    extern const uint8_t Cambridge26[];
+    extern const uint8_t Cambridge27[];
+    extern const uint8_t Cambridge28[];
+    extern const uint8_t Cambridge29[];
+    extern const uint8_t Cambridge30[];
+    extern const uint8_t Cambridge31[];
+    extern const uint8_t Cambridge32[];
+    extern const uint8_t Cambridge33[];
+    extern const uint8_t Cambridge34[];
+    extern const uint8_t Cambridge35[];
+    extern const uint8_t Cambridge36[];
+    extern const uint8_t Cambridge37[];
+    extern const uint8_t Cambridge38[];
+    extern const uint8_t Cambridge39[];
+    extern const uint8_t Cambridge40[];
+};
+#endif
